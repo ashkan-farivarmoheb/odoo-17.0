3414d98d61dc58225dfeaee594fd3a4c3d6187c3
Faraz <Farazgoli@gmail.com>
2024-06-28 17:01:24 +1000

    update create label url host {{request.host}}

--- extra-addons/theme_silon/static/src/js/navbar_scroll.js
+++ extra-addons/theme_silon/static/src/js/navbar_scroll.js
@@ -2,12 +2,17 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.NavbarScroll = publicWidget.Widget.extend({
-    selector: 'main',
+    selector: 'header',
     start: function () {
         console.log("Navbar Scroll JavaScript is loaded and running!");
 
         const onScroll = () => {
             console.log("Inside Scroll Event Handler!");
+            if (window.scrollY > 0) {
+                this.$el.addClass('scrolled');
+            } else {
+                this.$el.removeClass('scrolled');
+            }
         };
 
         // Attach the scroll event handler to the window
