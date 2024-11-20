48b70cd16de45a2b67319a3b76b67565eec68305
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-11-21 00:34:09 +1100

    integrate with nginx

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -1,14 +1,6 @@
-resource "aws_route53_zone" "private_zone" {
-  name = "erp.tisol.com.au"
-  vpc {
-    vpc_id = data.aws_vpcs.odoo.ids[0]
-  }
-  private_zone = true
-}
-
 resource "aws_route53_record" "tisol_nlb_cname" {
-  zone_id = aws_route53_zone.private_zone.id
-  name    = "erp.tisol.com.au"
+  zone_id = data.aws_route53_zone.tisol_com_au_zone.id
+  name    = "${var.environment}-${var.project}.awsnp.tisol.com.au.csr"
   type    = "CNAME"
   ttl     = 60
 
@@ -17,15 +9,4 @@ resource "aws_route53_record" "tisol_nlb_cname" {
   ]
 
   depends_on = [ aws_ecs_service.ecs_service, aws_lb.aws_lb_nlb ]
-}
-
-resource "aws_route53_record" "public_to_internal" {
-  zone_id = data.aws_route53_zone.public_zone.zone_id
-  name    = "erp.tisol.com.au"
-  type    = "CNAME"
-  ttl     = 60
-
-  records = [
-    aws_route53_record.aws_lb_nlb.fqdn
-  ]
 }
