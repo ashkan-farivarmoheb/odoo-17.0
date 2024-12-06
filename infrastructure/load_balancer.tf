37e56856feebcfd653f4ce378afede12b5ef7307
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2024-12-06 15:21:07 +1100

    fixing communication btw odoo and nginx

--- infrastructure/load_balancer.tf
+++ infrastructure/load_balancer.tf
@@ -12,10 +12,25 @@ resource "aws_lb" "aws_lb_nlb" {
 
 resource "aws_lb_target_group" "http_tg" {
   name     = "http-tg"
-  target_type = "ip"
+  target_type = "ip" # Use "instance" if targets are EC2 instances.
+  port        = 80
+  protocol    = "TCP"
   vpc_id = data.aws_vpcs.odoo.ids[0]
-  port     = 80
-  protocol = "TCP"
+
+  health_check {
+    port               = "traffic-port"
+    protocol           = "HTTP"
+    path               = "/health"
+    interval           = 30
+    timeout            = 5
+    healthy_threshold  = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project
+  }
 }
 
 resource "aws_lb_listener" "http_listener" {
@@ -35,6 +50,15 @@ resource "aws_lb_target_group" "tcp_tg" {
   vpc_id = data.aws_vpcs.odoo.ids[0]
   port     = 443
   protocol = "TCP"
+  health_check {
+    enabled             = true
+    port = "traffic-port"
+    protocol            = "TCP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+  }
 }
 
 resource "aws_lb_listener" "tcp_listener" {
